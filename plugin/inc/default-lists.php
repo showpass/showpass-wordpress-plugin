1aa617760b9f5319c67be836cf2ca2449e95676d
SuJiLee <suji@showpass.com>
2018-02-06 16:58:24 -0700

    - add "showpass" in style class names - css indentation fixed - remove padding/margin styles from php & add to styles - remove unnecessary class names that does not have any styles - tested with non-showpass themes - pagination working properly in different themes.

--- plugin/inc/default-lists.php
+++ plugin/inc/default-lists.php
@@ -1,51 +1,51 @@
-<div class="flex-box">
-	<div class="layout-flex">
+<div class="showpass-flex-box">
+	<div class="showpass-layout-flex">
 			<?php
 			$event_data = json_decode($data, true);
 			if ($event_data['count'] > 0) {
 				$events = $event_data['results'];
 				foreach ($events as $key => $event) { ?>
-				<div class="flex-50 flex-column no-border showpass-event-card ">
-					<div class="showpass-event-list layout-flex m15">
-						<div class="flex-100 flex-column no-border no-padding p0">
+				<div class="flex-50 showpass-flex-column showpass-no-border showpass-event-card">
+					<div class="showpass-event-list showpass-layout-flex m15">
+						<div class="flex-100 showpass-flex-column showpass-no-border showpass-no-padding p0">
 							<div>
 								<a href="/<?php if($detail_page){ print_r($detail_page);} else {print_r('event-detail');} ?>/?slug=<?php echo $event['slug']; ?>" class="showpass-image" >
 									<img alt="<?php echo $event['name']; ?>" src="<?php echo $event['image_stretch_banner_lg']; ?>"/>
 								</a>
 							</div>
 						</div>
-						<div class="flex-100 flex-column no-border background-white">
-							<div>
-								<div class="layout-flex">
-									<div class="flex-100 flex-column no-border">
+						<div class="flex-100 showpass-flex-column showpass-no-border showpass-background-white">
+							<div class="showpass-full-width">
+								<div class="showpass-layout-flex">
+									<div class="flex-100 showpass-flex-column showpass-no-border">
 										<div><?php if ($event['ticket_types']) : ?><small class="text-warning"> <?php $price = $event['ticket_types'];  print_r(showpass_get_price_range($price));?></small><?php endif; ?></div>
 									</div>
 								</div>
-								<div class="layout-flex">
-									<div class="flex-100 flex-column no-border">
+								<div class="showpass-layout-flex">
+									<div class="flex-100 showpass-flex-column showpass-no-border showpass-title-wrapper">
 										<div class="showpass-event-title">
-											<h3 class="mb0"><a href="/<?php if($detail_page){ print_r($detail_page);} else {print_r('event-detail');} ?>/?slug=<?php echo $event['slug']; ?>"><?php echo $event['name']; ?></a></h3>
+											<h3><a href="/<?php if($detail_page){ print_r($detail_page);} else {print_r('event-detail');} ?>/?slug=<?php echo $event['slug']; ?>"><?php echo $event['name']; ?></a></h3>
 										</div>
 									</div>
 								</div>
-								<div class="layout-flex">
-									<div class="flex-100 flex-column no-border">
+								<div class="showpass-layout-flex">
+									<div class="flex-100 showpass-flex-column showpass-no-border">
 										<div>
 											<div class="text-muted small"><?php echo showpass_get_event_date($event['starts_on'], $event['timezone'], false);?></div>
 											<div class="text-muted small"><?php $location = $event['location']; echo $location['name'];?></div>
 										</div>
 									</div>
 								</div>
-								<div class="layout-flex">
-										<div class="clearfix layout-flex showpass-list-button-layout">
-											<div class="flex-50 flex-column no-border button-pull-left">
+								<div class="showpass-showpass-layout-flex">
+										<div class="clearfix showpass-layout-flex showpass-list-button-layout">
+											<div class="flex-50 showpass-flex-column showpass-no-border showpass-button-pull-left">
 												<div class="w100">
-													<a class="list-ticket-button showpass-button" onclick="showpass.tickets.eventPurchaseWidget('<?php echo $event['slug'];?>', {'theme-primary': '#000000'})">Buy Tickets</a>
+													<a class="showpass-list-ticket-button showpass-button" onclick="showpass.tickets.eventPurchaseWidget('<?php echo $event['slug'];?>', {'theme-primary': '#000000'})">Buy Tickets</a>
 												</div>
 											</div>
-											<div class="flex-50 flex-column no-border button-pull-right">
+											<div class="flex-50 showpass-flex-column showpass-no-border showpass-button-pull-right">
 												<div class="w100">
-													<a class="list-ticket-button secondary showpass-button-secondary" href="/<?php if($detail_page){ print_r($detail_page);} else {print_r('event-detail');} ?>/?slug=<?php echo $event['slug']; ?>">More info</a>
+													<a class="showpass-list-ticket-button showpass-button-secondary" href="/<?php if($detail_page){ print_r($detail_page);} else {print_r('event-detail');} ?>/?slug=<?php echo $event['slug']; ?>">More info</a>
 												</div>
 											</div>
 										</div>
@@ -56,8 +56,8 @@
 				</div>
 			<?php } ?>
 			<?php if ($event_data['num_pages'] > 1) { ?>
-			<div class="flex-100 flex-column no-border text-center pagination-container">
-				<ul class="pagination showpass-pagination mb0 mt30">
+			<div class="flex-100 showpass-flex-column showpass-no-border text-center">
+				<ul class="showpass-pagination mb0 mt30">
 					<?php for ($i = 1; $i <= $event_data['num_pages']; $i++) {
 						$current = $i == $event_data['page_number'] ? 'class="current"' : '';
 						if ($current != '') { ?>
