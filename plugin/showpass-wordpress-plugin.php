cc70df53870f4fd45b4fdeb383d2371bdb2391f1
Marc Eshleman <marc@showpass.com>
2018-03-30 17:22:49 -0600

    update version number

--- plugin/showpass-wordpress-plugin.php
+++ plugin/showpass-wordpress-plugin.php
@@ -4,7 +4,7 @@
      Plugin URI:
      Description: Display events and event details, use the widget for on site ticket purchases
      Author: Showpass
-     Version: 2.6.2
+     Version: 2.6.4
      Author URI:
      */
 
