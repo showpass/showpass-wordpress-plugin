9626544fb81a6af177cf3bb8b022c11f36693af9
Marc Eshleman <marc@showpass.com>
2020-12-10 12:57:45 -0700

    make sure new event is not draft

--- plugin/showpass-wordpress-custom-api.php
+++ plugin/showpass-wordpress-custom-api.php
@@ -34,18 +34,28 @@ add_action('rest_api_init', function() {
  */
 function create_showpass_event($event_url) {
 	$token = get_option('option_showpass_access_token');
-  $request_url = 'https://local.showpass.com:9000/api/venue/22/events/';
+	// If no token then return error
+	if ( !$token ) {
+		return rest_ensure_response(wp_send_json_error('Error: Please enter a valid Showpass Event URL', $status_code = 400));
+	}
+  	$request_url = 'https://local.showpass.com:9000/api/venue/22/events/';
+
+	$ticket_type = array(
+		'name' => 'Placeholder Ticket Type',
+		'inventory' => 100,
+		'price' => 1000
+	);
 
 	$body = json_encode([
-		'name' => 'New Event request from Bits + Pieces', // event name [required]
+		'name' => 'New Event request from Bits + Pieces',
 		'venue' => 22,
-		'description' => 'this is a Test Event', // event description
-		'starts_on' => '2021-07-01T05:53:32.355328+00:00', // event start utc [required]
-		'ends_on' => '2021-07-01T06:53:32.355349+00:00', // event end utc [required]
-		'timezone' => 'Canada/Mountain', // [required]
-		'address' => 'My Address', // event address [required]
+		'location' => 16,
+		'description' => 'Change MEH',
+		'starts_on' => '2021-07-01T05:53:32.355328+00:00',
+		'ends_on' => '2021-07-01T06:53:32.355349+00:00',
+		'timezone' => 'Canada/Mountain',
 		'visibility' => 2,
-		'external_link' => $event_url
+		'external_link' => $event_url,
 	]);
 
 	$request = wp_remote_post($request_url, array(
@@ -68,7 +78,7 @@ function create_showpass_event($event_url) {
 		return rest_ensure_response(wp_send_json_error($request['body'], $status_code = 400));
 		#return wp_send_json_error($error_message, $status_code = 400);
 	} else {
-    $data = json_decode($request['body']);
+    	$data = json_decode($request['body']);
 		return rest_ensure_response( $data->slug );
 	}
 }
@@ -79,15 +89,15 @@ function create_showpass_event($event_url) {
 function showpass_api_process_url($data) {
   $url = esc_url_raw($data['url']);
 	$validURL = wp_http_validate_url($url);
-  $showpassEvent = strpos($validURL, 'showpass.com');
+  	$showpassEvent = strpos($validURL, 'showpass.com');
 
 
-	if ($validURL && $showpassEvent) {
+	if ( $validURL && $showpassEvent ) {
     // If the url is valid and showpass return slug
 		$splitURL = explode('/', $url);
 		$slug = $splitURL[3];
 		return rest_ensure_response($slug);
-	} else if ($validURL && !$showpassEvent) {
+	} else if ( $validURL && !$showpassEvent ) {
     // If the url is valid and NOT showpass create event
 		$create_event = create_showpass_event($url);
 		return $create_event;
