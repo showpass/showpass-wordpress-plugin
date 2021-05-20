3d3ee1978846e041f7b78cd2c7aea7d1fb1b49db
David Ma <david.ma@showpass.com>
2021-05-20 16:02:28 -0600

    refactor: add tags to old calendar widget

--- plugin/js/showpass-custom.js
+++ plugin/js/showpass-custom.js
@@ -94,7 +94,8 @@
 			let id = $(this).attr('data-org-id');
 			let params = {
 				'theme-primary': $(this).attr('data-color') || $('#option_widget_color').val(),
-				'keep-shopping': false
+				'keep-shopping': false,
+                'tags': $(this).attr('data-tags')
 			};
 
 			showpass.tickets.calendarWidget(id, params);
