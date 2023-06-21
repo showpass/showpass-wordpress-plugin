46f6acec84b0d3cc4581ea4f760a00d863bd7f6f
Kyle Milloy <k@fanda.io>
2023-06-21 12:20:51 -0600

    update verbiage

--- plugin/inc/button-verbiage.php
+++ plugin/inc/button-verbiage.php
@@ -1,17 +1,19 @@
-<?php if ($event['initiate_purchase_button'] === 'ipbd_buy_tickets') { ?>
+<?php if ($event['initiate_purchase_button'] === 'ipbd_buy_tickets'): ?>
   BUY TICKETS
-<?php } else if ($event['initiate_purchase_button'] === 'ipbd_register') { ?>
+<?php elseif ($event['initiate_purchase_button'] === 'ipbd_register'): ?>
   REGISTER
-<?php } else if ($event['initiate_purchase_button'] === 'ipbd_buy_passes') { ?>
+<?php elseif ($event['initiate_purchase_button'] === 'ipbd_buy_passes'): ?>
   BUY PASSES
-<?php } else if ($event['initiate_purchase_button'] === 'ipbd_select_date') { ?>
+<?php elseif ($event['initiate_purchase_button'] === 'ipbd_select_date'): ?>
   SELECT DATE
-<?php } else if ($event['initiate_purchase_button'] === 'ipbd_select_time') { ?>
+<?php elseif ($event['initiate_purchase_button'] === 'ipbd_select_time'): ?>
   SELECT TIME
-<?php } else if ($event['initiate_purchase_button'] === 'ipbd_select_seats') { ?>
-  SELECT SEATS
-<?php } else if ($event['initiate_purchase_button'] === 'ipbd_rsvp') { ?>
+<?php elseif ($event['initiate_purchase_button'] === 'ipbd_rsvp'): ?>
   RSVP
-<?php } else { ?>
+<?php elseif ($event['initiate_purchase_button'] === 'ipbd_get_tickets'): ?>
+  GET TICKETS
+<?php elseif ($event['initiate_purchase_button'] === 'ipbd_select_seats'): ?>
+  SELECT SEATS
+<?php else: ?>
   BUY TICKETS
-<?php } ?>
+<?php endif ?>
