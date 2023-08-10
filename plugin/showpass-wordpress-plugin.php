270ff609a33e0462da30ee8c9f60968893fef4e2
Marc Eshleman <marc@showpass.com>
2023-08-10 12:33:42 -0600

    SWP-8936 | fix query parameter issue (#43)
    * sync with SVN
    
    * SPW-8936 - Fix param not found issue for auto url query param
    
    * update version numbers

--- plugin/showpass-wordpress-plugin.php
+++ plugin/showpass-wordpress-plugin.php
@@ -1,12 +1,13 @@
 <?php
-    /*
-     Plugin Name: Showpass
-     Plugin URI: https://github.com/showpass/showpass-wordpress-plugin
-     Description: List events, display event details and products. Use the Showpass purchase widget for on site ticket & product purchases all with easy to use shortcodes. See our git repo here for full documentation. https://github.com/showpass/showpass-wordpress-plugin
-     Author: Showpass / Up In Code Inc.
-     Version: 3.8.2
-     Author URI: https://www.showpass.com
-     */
+
+/*
+ Plugin Name: Showpass
+ Plugin URI: https://github.com/showpass/showpass-wordpress-plugin
+ Description: List events, display event details and products. Use the Showpass purchase widget for on site ticket & product purchases all with easy to use shortcodes. See our git repo here for full documentation. https://github.com/showpass/showpass-wordpress-plugin
+ Author: Showpass / Up In Code Inc.
+ Version: 3.8.5
+ Author URI: https://www.showpass.com
+ */
 
 if (! defined('ABSPATH')) {
     exit;
@@ -19,38 +20,40 @@ if (! defined('ABSPATH')) {
 /**
  * imports and sets Showpass\ImageFormatter whenever a frontend template is loaded.
  */
-add_action( 'template_redirect', function() {
-    require_once plugin_dir_path( __FILE__ ) . 'inc/image-formatter.class.php';
+add_action('template_redirect', function () {
+    require_once plugin_dir_path(__FILE__) . 'inc/image-formatter.class.php';
     global $showpass_image_formatter;
     $showpass_image_formatter = new Showpass\ImageFormatter();
-    add_shortcode( 'showpass_events', 'showpass_get_event_data' );
-    add_shortcode( 'showpass_products', 'showpass_get_product_data' );
-    add_shortcode( 'showpass_calendar','showpass_display_calendar');
-    add_shortcode( 'showpass_widget', 'showpass_widget_expand');
-    add_shortcode( 'showpass_pricing_table', 'wpshp_get_pricing_table' );
+    add_shortcode('showpass_events', 'showpass_get_event_data');
+    add_shortcode('showpass_products', 'showpass_get_product_data');
+    add_shortcode('showpass_calendar', 'showpass_display_calendar');
+    add_shortcode('showpass_widget', 'showpass_widget_expand');
+    add_shortcode('showpass_pricing_table', 'wpshp_get_pricing_table');
 });
 
-function wpshp_admin_menu() {
+function wpshp_admin_menu()
+{
     /* create new top-level menu */
     add_menu_page('Showpass Events API', 'Showpass API', 'administrator', __FILE__, 'wpshp_settings_page', plugins_url('/images/icon.png', __FILE__));
 }
 
 add_action('admin_menu', 'wpshp_admin_menu');
 
-function register_wpshp_settings() {
-	/* register our settings */
-	register_setting('wpshp-settings-group', 'option_organization_id');
-	register_setting('wpshp-settings-group', 'option_widget_color');
-	register_setting('wpshp-settings-group', 'format_date');
-	register_setting('wpshp-settings-group', 'format_time');
-	register_setting('wpshp-settings-group', 'option_theme_dark');
-	register_setting('wpshp-settings-group', 'option_keep_shopping');
-	register_setting('wpshp-settings-group', 'option_show_widget_description');
-	register_setting('wpshp-settings-group', 'option_disable_verify_ssl');
+function register_wpshp_settings()
+{
+    /* register our settings */
+    register_setting('wpshp-settings-group', 'option_organization_id');
+    register_setting('wpshp-settings-group', 'option_widget_color');
+    register_setting('wpshp-settings-group', 'format_date');
+    register_setting('wpshp-settings-group', 'format_time');
+    register_setting('wpshp-settings-group', 'option_theme_dark');
+    register_setting('wpshp-settings-group', 'option_keep_shopping');
+    register_setting('wpshp-settings-group', 'option_show_widget_description');
+    register_setting('wpshp-settings-group', 'option_disable_verify_ssl');
     register_setting('wpshp-settings-group', 'option_use_showpass_beta');
-	register_setting('wpshp-settings-group', 'option_showpass_access_token');
-	register_setting('wpshp-settings-group', 'option_showpass_distribution_tracking');
-  	register_setting('wpshp-settings-group', 'option_showpass_default_button_class');
+    register_setting('wpshp-settings-group', 'option_showpass_access_token');
+    register_setting('wpshp-settings-group', 'option_showpass_distribution_tracking');
+    register_setting('wpshp-settings-group', 'option_showpass_default_button_class');
 }
 
 /* call register settings function */
